8d410cc2f8ec67a80f96f28ca31bb4cf923b416c
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-07-25 22:32:31 +0300

    Yanlışlıkla ekelenen enum silindi.

--- uCAN/Inc/ucan_macros.h
+++ uCAN/Inc/ucan_macros.h
@@ -3,8 +3,6 @@
 
 #include "ucan_types.h"
 
-#define UCAN_MAX_PACKET_COUNT 128
-
 #define UCAN_PACKET_COUNT(list) (sizeof(list) / sizeof((list)[0]))
 
 #define UCAN_CLIENT_COUNT(client) (sizeof(client) / sizeof((client)[0]))
@@ -32,12 +30,5 @@
 #define IS_UCAN_DATA_TYPE(TYPE) (((TYPE) == UCAN_U8) || \
                          	 ((TYPE) == UCAN_U16) || \
 							 ((TYPE) == UCAN_U32)
-
-
-typedef enum {
-    UCAN_U8,
-    UCAN_U16,
-    UCAN_U32
-} UCAN_DataType;
 
 #endif
