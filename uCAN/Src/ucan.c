add7b8b9c78eae57feb48650d4ca00109a98ea35
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-07-27 04:32:19 +0300

    Filtre için defualt ayarlar eklendi. Debug prefix uyumu sağlandı.

--- uCAN/Src/ucan.c
+++ uCAN/Src/ucan.c
@@ -3,6 +3,17 @@
 #include "ucan.h"
 #include "ucan_debug.h"
 
+const CAN_FilterTypeDef defaultFilterConfig = {
+    .FilterMode = CAN_FILTERMODE_IDMASK,
+    .FilterFIFOAssignment = CAN_FILTER_FIFO0,
+    .FilterIdHigh = 0x0000,
+    .FilterIdLow = 0x0000,
+    .FilterMaskIdHigh = 0x0000,
+    .FilterMaskIdLow = 0x0000,
+    .FilterScale = CAN_FILTERSCALE_32BIT,
+    .FilterActivation = CAN_FILTER_ENABLE
+};
+
 UCAN_StatusTypeDef uCAN_Init(UCAN_HandleTypeDef* ucan)
 {
 	if(ucan->hcan == NULL)
@@ -11,14 +22,25 @@ UCAN_StatusTypeDef uCAN_Init(UCAN_HandleTypeDef* ucan)
 		return UCAN_INVALID_PARAM;
 	}
 
+	if(ucan->filter.FilterActivation == CAN_FILTER_DISABLE)
+	{
+		ucan->filter = defaultFilterConfig;
+	}
+
+
 	ucan->status = UCAN_OK;
 	return UCAN_OK;
 }
 
 UCAN_StatusTypeDef uCAN_Start(UCAN_HandleTypeDef* ucan, UCAN_Config* config)
 {
-	UCAN_StatusTypeDef txListCheck = uCAN_CheckPacketConfig(config->txPacketList, &ucan->txHolder);
-	UCAN_StatusTypeDef rxListCheck = uCAN_CheckPacketConfig(config->rxPacketList, &ucan->rxHolder);
+	if(ucan->status == UCAN_NOT_INITIALIZED)
+	{
+		return UCAN_NOT_INITIALIZED;
+	}
+
+	UCAN_StatusTypeDef txListCheck = uCAN_Debug_CheckPacketConfig(config->txPacketList, &ucan->txHolder);
+	UCAN_StatusTypeDef rxListCheck = uCAN_Debug_CheckPacketConfig(config->rxPacketList, &ucan->rxHolder);
 
 	if (txListCheck != UCAN_OK)
 	{
@@ -32,12 +54,12 @@ UCAN_StatusTypeDef uCAN_Start(UCAN_HandleTypeDef* ucan, UCAN_Config* config)
 		return rxListCheck;
 	}
 
-	uCAN_Finalize_Packet(config->txPacketList, &ucan->txHolder);
+	uCAN_Debug_FinalizePacket(config->txPacketList, &ucan->txHolder);
 
-	uCAN_Finalize_Packet(config->rxPacketList, &ucan->rxHolder);
+	uCAN_Debug_FinalizePacket(config->rxPacketList, &ucan->rxHolder);
 
 
-	//HAL_CAN_Start(ucan->hcan);
+
 
 	ucan->status = UCAN_OK;
 	return UCAN_OK;
@@ -50,6 +72,8 @@ UCAN_StatusTypeDef uCAN_SendAll(UCAN_HandleTypeDef* ucan)
 		return UCAN_NOT_INITIALIZED;
 	}
 
+	//TODO
+
 	return UCAN_OK;
 }
 
@@ -60,6 +84,9 @@ UCAN_StatusTypeDef uCAN_Update(UCAN_HandleTypeDef* ucan)
 		return UCAN_NOT_INITIALIZED;
 	}
 
+
+	//TODO
+
 	return UCAN_OK;
 }
 
@@ -69,6 +96,8 @@ UCAN_StatusTypeDef uCAN_Handshake(UCAN_HandleTypeDef* ucan)
 	{
 		return UCAN_NOT_INITIALIZED;
 	}
+
+	//TODO
 
 	return UCAN_OK;
 }
