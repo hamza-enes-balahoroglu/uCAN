dde7bc8195b12d11978b5028285746b0f3f37de9
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-07-28 15:42:31 +0300

    Update fonksiyonu global olarak .h dosyasına tanımlandı.

--- uCAN/Inc/ucan_runtime.h
+++ uCAN/Inc/ucan_runtime.h
@@ -6,5 +6,7 @@
 
 UCAN_StatusTypeDef uCAN_Runtime_SendPacket(CAN_HandleTypeDef* hcan, UCAN_Packet* packet);
 
+UCAN_StatusTypeDef uCAN_Runtime_UpdatePacket(UCAN_HandleTypeDef* ucan);
+
 int uCAN_Runtime_ComparePacketId(const void* a, const void* b);
 #endif
